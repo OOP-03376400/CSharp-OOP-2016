3b0eebae7707c755cea676ccc3da807325f47257
stefanovnm <stefanov.nm@gmail.com>
2016-06-10 15:51:24 +0300

    rename

---
+++ CSharp-OOP-01-DefClasses-P1/Homework/GSMTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework
+{
+    using System;
+
+    class GSMTest
+    {
+        static void Main(string[] args)
+        {
+            GSM myGSM = new GSM("One Plus", "X");
+            myGSM.Battery = new Battery("non-removable", 2525, BatteryType.LiPo);
+            myGSM.Owner = "me";
+            //Console.WriteLine(myGSM.ToString());
+            //Console.WriteLine(GSM.IPhone4s);
+            GSM iPhone6 = new GSM("Apple", "Iphone6");
+            GSM samsungS7 = new GSM("Samsung", "S7");
+
+            GSM[] allGSMs = new GSM[] {myGSM,GSM.IPhone4s, iPhone6, samsungS7 };
+            foreach (var gsm in allGSMs)
+            {
+                Console.WriteLine(gsm);
+            }
+        }
+    }
+}
