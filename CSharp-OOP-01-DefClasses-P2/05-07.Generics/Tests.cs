ed7c6b84fb2216604a2ad95d9c99bf7b5a414e08
stefanovnm <stefanov.nm@gmail.com>
2016-06-13 18:55:20 +0300

    Task 5: Add, AddAtPosition, Remove, Clear implemented

---
+++ CSharp-OOP-01-DefClasses-P2/05-07.Generics/Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace _05_07.Generics
+{
+    using System;
+
+    public class Tests
+    {
+        public static void Main()
+        {
+            GenericList<int> firstList = new GenericList<int>();
+            Console.WriteLine(firstList);
+
+            //testing Add
+            firstList.Add(31);
+            Console.WriteLine(firstList);
+
+            //testing AddAtPosition
+            firstList.AddAtPosition(52,0);
+            Console.WriteLine(firstList);
+
+            //testing Remove
+            firstList.Remove(0);
+            Console.WriteLine(firstList);
+
+            //testing Clear
+            firstList.Clear();
+            Console.WriteLine(firstList);
+        }
+    }
+}
