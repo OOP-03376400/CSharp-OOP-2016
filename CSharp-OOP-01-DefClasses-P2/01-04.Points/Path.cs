fd0e22690f2bb812422317e62cb1486ecaf50cb4
stefanovnm <stefanov.nm@gmail.com>
2016-06-13 16:24:03 +0300

    Task 1, 2 and 3 done

---
+++ CSharp-OOP-01-DefClasses-P2/01-04.Points/Path.cs
@@ -0,0 +1,14 @@
+﻿//Create a class Path to hold a sequence of points in the 3D space.
+//Create a static class PathStorage with static methods to save and load paths from a text file.
+//Use a file format of your choice.
+
+
+namespace _01_04.Points
+{
+    using System;
+
+    public class Path
+    {
+
+    }
+}
---
+++ CSharp-OOP-01-DefClasses-P2/01-04.Points/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace _01_04.Points
+{
+    using System;
+
+    public class Tests
+    {
+        public static void Main()
+        {
+            var point = new Point3D();
+
+            point.X = 1;
+            point.Y = 1;
+            point.Z = 1;
+
+            Console.WriteLine(point);
+            Console.WriteLine(Point3D.O);
+
+
+            Console.WriteLine("Distance between " + point + " and O is "+Distance.Between(point, Point3D.O));
+        }
+    }
+}
