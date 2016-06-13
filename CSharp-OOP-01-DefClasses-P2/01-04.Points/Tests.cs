b8ddbdc1b2e2268e6386ad65b8aba4e1a6498e51
stefanovnm <stefanov.nm@gmail.com>
2016-06-13 17:50:15 +0300

    Task 1 to 4 are finished

--- CSharp-OOP-01-DefClasses-P2/01-04.Points/Tests.cs
+++ CSharp-OOP-01-DefClasses-P2/01-04.Points/Tests.cs
@@ -8,15 +8,31 @@
         {
             var point = new Point3D();
 
-            point.X = 1;
-            point.Y = 1;
-            point.Z = 1;
+            point.X = 0.5M;
+            point.Y = -3;
+            point.Z = 1.35M;
 
             Console.WriteLine(point);
             Console.WriteLine(Point3D.O);
-
-
             Console.WriteLine("Distance between " + point + " and O is "+Distance.Between(point, Point3D.O));
+
+            var path = new Path();
+            path.AddPoint(Point3D.O);
+            path.AddPoint(point);
+
+            Console.WriteLine(path);
+
+            var secondPath = new Path(new Point3D(1, 2, 3), new Point3D(2, 3, 4), new Point3D(3, 4, 5));
+
+            Console.WriteLine(secondPath);
+
+            PathStorage.SaveToFile("export.txt",secondPath);
+
+            Console.WriteLine();
+
+            Path pathFromFile = PathStorage.ReadFromFile("import.txt");
+            Console.WriteLine(pathFromFile);
+
         }
     }
 }
